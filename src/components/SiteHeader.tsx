d4c2e2c6774c3d95241ed5f8258d7b51cb4011ee
Arman Tajalli <armantajalli@gmail.com>
2026-04-30 19:05:41 -0400

    reset top home page button

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import type React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { UserButton, useClerk, useUser } from "@clerk/nextjs";
